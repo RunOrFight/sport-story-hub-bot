d9da181f7677888898ea58c387958be8069d0c6b
Ignat Mustafin <ignatmustafin77@gmail.com>
2023-12-01 10:45:24 +0300

    #2 added postgres

--- src/database/data-source-local.ts
+++ src/database/data-source-local.ts
@@ -1,17 +1,28 @@
-import { DataSource } from "typeorm";
-import { DataSourceOptions } from "typeorm/data-source/DataSourceOptions";
+import {DataSource} from "typeorm";
+import {DataSourceOptions} from "typeorm/data-source/DataSourceOptions";
 
-let connectionOptions: DataSourceOptions = {
-    type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "admin", // postgre username
-    database: "sport_hub", // postgre database, needs to be created before
-    synchronize: false, // if true, you don't really need migrations
-    logging: true,
-    entities: ["src/database/entities/**/*.ts"], // where our entities reside
-    migrations: ["src/database/migrations/*.ts"], // where our migrations reside
-};
+let connectionOptions: DataSourceOptions = process.env.NODE_ENV === "production" ? {
+        type: "postgres",
+        host: "postgres",
+        port: 5432,
+        username: "admin", // postgre username
+        database: "sport_hub", // postgre database, needs to be created before
+        synchronize: false, // if true, you don't really need migrations
+        logging: true,
+        entities: ["src/database/entities/**/*.ts"], // where our entities reside
+        migrations: ["src/database/migrations/*.ts"], // where our migrations reside
+    } :
+    {
+        type: "postgres",
+        host: "localhost",
+        port: 5432,
+        username: "admin", // postgre username
+        database: "sport_hub", // postgre database, needs to be created before
+        synchronize: false, // if true, you don't really need migrations
+        logging: true,
+        entities: ["src/database/entities/**/*.ts"], // where our entities reside
+        migrations: ["src/database/migrations/*.ts"], // where our migrations reside
+    };
 
 export default new DataSource({
     ...connectionOptions,
