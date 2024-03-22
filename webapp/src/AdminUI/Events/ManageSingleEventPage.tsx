126989601b32eb39cf4dc1ab6a47c6391041df7b
RunOrFight <dragoinfury@gmail.com>
2024-03-22 23:35:15 +0300

    single event games

--- webapp/src/AdminUI/Events/ManageSingleEventPage.tsx
+++ webapp/src/AdminUI/Events/ManageSingleEventPage.tsx
@@ -1,12 +1,12 @@
 import {
   Button,
-  Card,
   Collapse,
   CollapseProps,
-  Empty,
   Flex,
   List,
   Popconfirm,
+  Segmented,
+  SegmentedProps,
   Space,
   Table,
   Typography,
@@ -17,70 +17,66 @@ import { eventsSlice } from "../../Store/Events/EventsSlice.ts";
 import { withProps } from "../../Utils/WithProps.ts";
 import { RequestStatusToComponent } from "../../Components/RequestStatusToComponent.tsx";
 import { EVENTS_GET_BY_ID_REQUEST_SYMBOL } from "../../Store/Events/EventsVariables.ts";
-import {
-  TEventGame,
-  TEventGameTeam,
-  TEventTeams,
-} from "../../Models/TEvent.ts";
+import { TEventGame, TEventTeams } from "../../Models/TEvent.ts";
 import { FC, useState } from "react";
 import { DeleteOutlined, EditOutlined, PlusOutlined } from "@ant-design/icons";
-import { isEmpty } from "../../Utils/OneLineUtils.ts";
 import { getNotNil } from "../../Utils/GetNotNil.ts";
 import { generatePath, Link } from "react-router-dom";
 import { webappRoutes } from "../../../../src/constants/webappRoutes.ts";
 import { teamParticipantRenderItem } from "./UserRenderItem.tsx";
 import { PARTICIPANT_TABLE_COLUMNS } from "./ParticipantTableColumns.tsx";
 
-const GameTeam: FC<TEventGameTeam> = ({ name, teamsParticipants }) => {
-  return (
-    <Flex vertical>
-      <Typography.Title level={5}>{name}</Typography.Title>
-
-      <Table
-        dataSource={teamsParticipants.map((it) => it.participant)}
-        columns={PARTICIPANT_TABLE_COLUMNS}
-      />
-    </Flex>
-  );
-};
-
 const Game: FC<TEventGame> = ({ gameTeams }) => {
-  return (
-    <Flex vertical>
-      {gameTeams.map(({ team }) => (
-        <GameTeam {...team} key={team.id} />
-      ))}
-    </Flex>
+  const dispatch = useDispatch();
+
+  const items: CollapseProps["items"] = gameTeams.map(
+    ({ team: { name, id, teamsParticipants } }) => ({
+      label: name,
+      key: `team_${id}`,
+      children: (
+        <List
+          dataSource={teamsParticipants}
+          renderItem={teamParticipantRenderItem(dispatch, id)}
+        />
+      ),
+    }),
   );
+
+  return <Collapse items={items} />;
 };
 
-const Games: FC<{ games: TEventGame[] }> = ({ games }) => {
-  const [activeTabKey, setActiveTabKey] = useState<string>(String(games[0].id));
+const EventGames: FC<{ games: TEventGame[] }> = ({ games }) => {
+  const [activeTabKey, setActiveTabKey] = useState<number>(games[0].id);
 
-  const onTabChange = (key: string) => {
-    setActiveTabKey(key);
+  const onChange: SegmentedProps<number>["onChange"] = (value) => {
+    if (value === -1) {
+      return;
+    }
+    setActiveTabKey(value);
   };
 
-  const tabList = games.map(({ name, id }) => ({
-    tab: name,
-    key: String(id),
-  }));
+  const gamesTabs: SegmentedProps<number>["options"] = games.map(
+    ({ name, id }) => ({
+      label: name,
+      value: id,
+    }),
+  );
+
+  const tabList: SegmentedProps<number>["options"] = gamesTabs.concat({
+    icon: <PlusOutlined />,
+    value: -1,
+  });
 
   const game = getNotNil(
-    games.find((it) => String(it.id) === activeTabKey),
-    "Games",
+    games.find((it) => it.id === activeTabKey),
+    "EventGames",
   );
 
   return (
-    <Card
-      style={{ width: "100%" }}
-      title={"Games"}
-      tabList={tabList}
-      onTabChange={onTabChange}
-      activeTabKey={activeTabKey}
-    >
+    <Flex vertical gap={16}>
+      <Segmented options={tabList} onChange={onChange} />
       <Game {...game} key={game.id} />
-    </Card>
+    </Flex>
   );
 };
 
@@ -160,18 +156,16 @@ const ManageSingleEventPageSuccess = () => {
 
       <EventTeams teams={teams} eventId={id} />
 
+      <Typography.Title level={4}>{"Games: "}</Typography.Title>
+
+      <EventGames games={games} />
+
       <Typography.Title level={4}>{"Participants: "}</Typography.Title>
       <Table
         dataSource={participants}
         columns={PARTICIPANT_TABLE_COLUMNS}
         showHeader={false}
       />
-
-      {isEmpty(games) ? (
-        <Empty description={"No games"} />
-      ) : (
-        <Games games={games} />
-      )}
     </Flex>
   );
 };
